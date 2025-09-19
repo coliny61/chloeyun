d253cf2d099d14d1ca3d608417188910dc8c026f
coliny61 <cy61biz@gmail.com>
2025-09-18 21:21:27 -0500

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,28 +1,12 @@
-import Hero from "./Hero";
-import Navigation from "./Navigation";
-import Footer from "./Footer";
-import PageHeader from "./PageHeader";
-import FoodReviews from "./FoodReviews";
-import Lifestyle from "./Lifestyle";
-import Events from "./Events";
-import Portfolio from "./Portfolio";
-import MediaKit from "./MediaKit";
-import Press from "./Press";
-import Contact from "./Contact";
+import React from 'react';
+import Hero from './components/Hero';
 
-export default function App() {
+function App() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-white text-black">
-      <Navigation />
-      <Hero />
-      <Portfolio />
-      <FoodReviews />
-      <Lifestyle />
-      <Events />
-      <MediaKit />
-      <Press />
-      <Contact />
-      <Footer />
+    <main>
+      <Hero onNavigate={(page) => console.log("Navigate to:", page)} />
     </main>
   );
 }
+
+export default App;
